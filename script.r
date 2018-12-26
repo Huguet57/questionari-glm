39bf48792d58f24fd4bd5cfb9f818f6fdd7dab43
Andreu <andreuhuguet@gmail.com>
2018-12-26 18:16:49 +0100

    v2

--- script.r
+++ script.r
@@ -1,4 +1,4 @@
-setwd("~/Downloads/pie2/desembre/")
+setwd("~/Downloads/pie2/desembre/questionari-glm")
 library(car)
 library(nlme)
 dd <- read.csv2("ah.csv")
@@ -15,6 +15,9 @@ summary(m1 <- glm(H~Days,
                   family=gaussian(link="sqrt"),
                   data=dd))
 
+plot(dd$Days, predict(m1, type="response"), type="l")
+with(dd, points(Days, H, col="red", pch="+"))
+
 # Ap. 2
 plot(m1, which=1)
 plot(rstandard(m1, type="pearson"))
@@ -28,7 +31,6 @@ m1f <- glm(H ~ Days + FDays,
            data = dd)
 
 print(anova(m1, m1f, test="F"))
-print(anova(m1f, test="F"))
 
 # Ap. 4
 print(leveneTest(resid(m1, type="pearson")~FDays))
@@ -48,8 +50,8 @@ summary(m2 <- glm(H ~ Days,
           family=Gamma(link="log"),
           data=dd))
 
-plot(predict(m2, ty="response"))
-with(dd, points(Days, H, col="red"))
+plot(dd$Days, predict(m2, type="response"), type="l")
+with(dd, points(Days, H, col="red", pch="+"))
 
 # Ap. 2
 plot(rstandard(m2), ylim = c(4,-4))
@@ -82,6 +84,9 @@ summary(m3 <- glm(H ~ Days,
                   family=quasi(variance="mu",link="log"),
                   data=dd))
 
+plot(dd$Days, predict(m3, type="response"), type="l")
+with(dd, points(Days, H, col="red", pch="+"))
+
 # Ap. 2
 plot(rstandard(m3))
 abline(h=c(-2,0,2))
@@ -94,5 +99,64 @@ which(abs(rstandard(m3)) > 2)
 # Ap. 5
 customDays <- data.frame(Days=c(0, 105, 150))
 pred <- predict(m3, customDays, se.fit = T, type="response")
-print(cbind(mu = pred$fit,
-            se = pred$se.fit))
+
+mu3 <- pred$fit
+phi3 <- summary(m3)$dispersion
+var_poisson <- phi3*mu3 # poisson: V(mu) = mu
+
+print(cbind(Var = var_poisson,
+            Days = customDays))
+
+# Ap. 6
+# question 17
+res.dev <- c()
+null.dev <- c()
+
+res.dev[1] <- sum(summary(m1)$deviance.resid^2)
+null.dev[1] <- summary(m1)$null.deviance
+
+res.dev[2] <- sum(summary(m2)$deviance.resid^2)
+null.dev[2] <- summary(m2)$null.deviance
+
+res.dev[3] <- sum(summary(m3)$deviance.resid^2)
+null.dev[3] <- summary(m3)$null.deviance
+
+cbind(mod = c(1,2,3),
+      res = res.dev,
+      null = null.dev,
+      ratio = res.dev/null.dev)
+
+# question 18
+leveneTest(resid(m1, type="pearson")~FDays)
+leveneTest(resid(m2, type="pearson")~FDays)
+leveneTest(resid(m3, type="pearson")~FDays)
+
+# question 19
+mu <- c()
+phi <- c()
+var <- c()
+
+customDays <- data.frame(Days=c(0, 150))
+
+for (day in customDays$Days) {
+  mu[1] <- predict(m1, data.frame(Days=c(day)), type="response")
+  mu[2] <- predict(m2, data.frame(Days=c(day)), type="response")
+  mu[3] <- predict(m3, data.frame(Days=c(day)), type="response")
+  
+  phi[1] <- summary(m1)$dispersion
+  phi[2] <- summary(m2)$dispersion
+  phi[3] <- summary(m3)$dispersion
+  
+  var[1] <- phi[1]*1 # Normal
+  var[2] <- phi[2]*mu[2]*mu[2] # Gamma
+  var[3] <- phi[3]*mu[3] # Poisson
+  
+  print(cbind(mu = mu,
+              disp = phi,
+              var = var))
+}
+  
+# plots
+plot(dd$Days, predict(m1, type="response"), type="l", col="blue")
+lines(dd$Days, predict(m2, type="response"), type="l", col="red", lty=1)
+lines(dd$Days, predict(m3, type="response"), type="l", col="green", lty=4)
